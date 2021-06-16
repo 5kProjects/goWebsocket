60bf3d552fc9511db3fbe35bdfb0116a1cb65e38
unknown <belay.bura@gmail.com>
2021-06-16 19:18:29 +0300

    checked the websocket works

---
+++ main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"flag"
+	"github.com/gorilla/websocket"
+
+	// "golang.org/x/net/trace"
+	"log"
+	"net/http"
+
+	"path/filepath"
+	"sync"
+	"text/template"
+	// "golang.org/x/net/trace"
+	//  "github.com/birukbelay/chatTrace/trace"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
+
+	r := NewRoom()
+	// r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
+
+	// start the web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
+}
+
+
+type room struct {
+
+	// forward is a channel that holds incoming messages
+	// that should be forwarded to the other clients.
+	forward chan []byte
+
+	// join is a channel for clients wishing to join the room.
+	join chan *client
+
+	// leave is a channel for clients wishing to leave the room.
+	leave chan *client
+
+	// clients holds all current clients in this room.
+	clients map[*client]bool
+
+	// tracer will receive trace information of activity
+	// in the room.
+	// tracer trace.Tracer
+}
+
+// newRoom makes a new room that is ready to
+// go.
+
+func NewRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+		//tracer:  trace.Off(),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+			// r.tracer.Trace("New client joined")
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+			// r.tracer.Trace("Client left")
+		case msg := <-r.forward:
+			//r.tracer.Trace("Message received: ", string(msg))
+			// forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+				// r.tracer.Trace(" -- sent to client")
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
+
+
+//----------------
+
+
+// client represents a single chatting user.
+type client struct {
+
+	// socket is the web socket for this client.
+	socket *websocket.Conn
+
+	// send is a channel on which messages are sent.
+	send chan []byte
+
+	// room is the room this client is chatting in.
+	room *room
+}
+
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
